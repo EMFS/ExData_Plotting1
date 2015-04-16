311cc2c65527731691f016c9abc409f75d563849
EMFS <EMFS@users.noreply.github.com>
2015-04-16 17:42:14 -0300

    updated URL of dataset

--- plot1.R
+++ plot1.R
@@ -1,6 +1,10 @@
 # First plot: histogram of the variable 'Global_active_power' for the Household power consumption data
 # on the days 2007-02-01 to 2007-02-02
 
+## Dataset used in this project is downloaded from:
+## https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+## and unzipped in the R Working directory
+
 # setwd("D:/workspace/DataScienceSpecial/EDA/assessment1/ExData_Plotting1")
 # Use data.table instead of data.frame
 library(data.table)
